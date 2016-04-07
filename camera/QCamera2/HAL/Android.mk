978f0dc82a212fe7fae7f84a3baff507ef41020f
Bharat Pawar <bpawar@codeaurora.org>
2016-04-07 21:33:40 +0530

    QCamera2: Disable CLANG
    Disabled CLANG.
    
    Change-Id: I7c807cfc8bb3aeeb3ade573177139c5f29027a13

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -73,6 +73,7 @@ LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_CLANG := false
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
 
