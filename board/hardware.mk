22871140e93677288b81dd8846d7e17467906e40
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-12-07 21:15:22 +0200

    lettuce: Add CMHW touchscreen gestures implementation
    Change-Id: Ifad247b964ac341c00cc9b43822c938c4b7396ca

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,2 +1,5 @@
+# CMHW
+BOARD_HARDWARE_CLASS += device/yu/lettuce/cmhw
+
 # Tap to wake
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/double_tap_enable"
