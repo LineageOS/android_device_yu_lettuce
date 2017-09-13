80243d64558128aaf21497bffb5fea21f02999f1
Scott Warner <Tortel1210@gmail.com>
2017-09-13 19:25:15 -0400

    lettuce: Switch to permissive
    Change-Id: Ia4c979c92f6c2c0f6ad384922b6f8d43a2d1b100

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,3 +4,4 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_lettuce_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
