cd5e7cae325f087e02cb9be209d5385378413d6e
jrior001 <jriordan001@gmail.com>
2018-02-06 17:52:39 -0500

    lettuce: cmhw -> lineagehw
    Change-Id: I5a409cad62f85a4894b09f077bc1348129b433f6

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,5 +1,5 @@
 # CMHW
-BOARD_HARDWARE_CLASS += device/yu/lettuce/cmhw
+BOARD_HARDWARE_CLASS += device/yu/lettuce/lineagehw
 
 # Tap to wake
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/double_tap_enable"
