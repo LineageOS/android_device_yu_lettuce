f52080f017244ec2203c856bc13f2d2463de6c63
Bruno Martins <bgcngm@gmail.com>
2018-08-10 18:47:00 +0530

    lettuce: Consider lineage_lettuce product via the AOSP way
    Change-Id: I804564c020e1fe394616dac5b0fe103153f26684

--- lineage_lettuce.mk
+++ lineage_lettuce.mk
@@ -1,18 +1,17 @@
-#
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -21,9 +20,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lettuce device
 $(call inherit-product, device/yu/lettuce/device.mk)
 
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lettuce
-PRODUCT_NAME := full_lettuce
+PRODUCT_NAME := lineage_lettuce
 PRODUCT_BRAND := YU
 PRODUCT_MODEL := YU5010
 PRODUCT_MANUFACTURER := YU
+BOARD_VENDOR := yu
+
+PRODUCT_GMS_CLIENTID_BASE := android-micromax
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="YUPHORIA-user 5.1.1 LMY49J YOG4PAS8A8 release-keys" \
+    PRODUCT_NAME=YUPHORIA \
+    TARGET_DEVICE=YUPHORIA
+
+BUILD_FINGERPRINT := YU/YUPHORIA/YUPHORIA:5.1.1/LMY49J/YOG4PAS8A8:user/release-keys
